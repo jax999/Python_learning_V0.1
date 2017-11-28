fa928239273c060daade96bedfbf161926366c2b
jax999 <noodle.lagomfart@outlook.com>
2017-11-28 16:25:53 +0100

    final master commit

--- test.py
+++ test.py
@@ -6,10 +6,15 @@ STRING = ['hello world']
 NAMES = ["john", "eric", "jessica"]
 
 # write your code here
-Second_name = ['eric']
+SECOND_NAME = ['eric']
 
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
 print(NUMBERS)
 print(STRING)
-print("The second name on the names list is %s" % Second_name)
+print("The second name on the names list is %s" % SECOND_NAME)
+# number games
+EVEN_NUMBERS = [2, 4, 6, 8]
+ODD_NUMBERS = [1, 3, 5, 7]
+ALL_NUMBERS = ODD_NUMBERS + EVEN_NUMBERS
+print(ALL_NUMBERS)
