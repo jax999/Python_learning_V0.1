39aa9b9add581a750d64f853b702be1157f2c5ca
jax999 <noodle.lagomfart@outlook.com>
2017-12-04 13:18:46 +0100

    minor change
    nothing special

--- test.py
+++ test.py
@@ -10,10 +10,13 @@ SECOND_NAME = ['eric']
 
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
+
 print(NUMBERS)
 print(STRING)
 print("The second name on the names list is %s" % SECOND_NAME)
+
 # number games
+
 EVEN_NUMBERS = [2, 4, 6, 8]
 ODD_NUMBERS = [1, 3, 5, 7]
 ALL_NUMBERS = ODD_NUMBERS + EVEN_NUMBERS
