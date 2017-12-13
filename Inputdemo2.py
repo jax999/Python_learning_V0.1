a6009e4c46940faea594b60d964adccfe46618b0
jax999 <noodle.lagomfart@outlook.com>
2017-12-13 08:17:43 +0100

    new demo file and updates

--- Inputdemo2.py
+++ Inputdemo2.py
@@ -1,10 +1,10 @@
 # Testing input variable
-""" Input variable demo2 """
+"""Input variable demo2"""
 print('\n')
-print('Godmorning! \n')
+print('Goodmorning! \n')
 
 print('Quote for today?\n')  # Quote variable
-Quote = input()
+# print('Input') + Quote = input()
 
 # Printing what is in input
 print('Todays quote is '+Quote)
