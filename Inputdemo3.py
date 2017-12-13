d99902fbe06449b339225e394c8791a256034249
jax999 <noodle.lagomfart@outlook.com>
2017-12-13 10:12:01 +0100

    correct pylint error

--- Inputdemo3.py
+++ Inputdemo3.py
@@ -1,8 +1,8 @@
 #Ask for age and how far you want to calculate
-print ('What is your age?')
+"""Stuff"""
+print('What is your age?')
 myage = input('Ange ålder\n')
 myage = int(myage)
 years = input('Ange hur långt den ska räkna\n')
 years = int(years)
-print (myage + years)
-
+print(myage + years)
