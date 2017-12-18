2456aa475096df7255794ec5323b4687df352b3b
jax999 <noodle.lagomfart@outlook.com>
2017-12-18 13:59:10 +0100

    code mess

--- Inputdemo3.py
+++ Inputdemo3.py
@@ -2,13 +2,18 @@
 """Stuff""" #Docline
 
 # Syntax for age and age ahead
-print('What is your age?\n\n')
-myage = input('Input age\n')
-myage = int(myage)
-
+    print('What is your age?\n')
+    myage = input('Input age\n')
+    myage = int(myage)
+    if myage == 40:
+        print ('not likely')
+        continue        
 # syntax for calculation
 years = input('Input how many years ahead you want it to calculate\n')
 years = int(years)
+
+
+    
 
 # Prints sum of syntax 3 och 7
 print('\n')
